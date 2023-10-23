9a9eb48da2598ca37efe9e4f18cd63cd7b870459
mihirat <teketen5656@gmail.com>
2023-10-23 10:14:53 +0900

    bridge, strategy

---
+++ src/main/bridge/CountDisplay.java
@@ -0,0 +1,15 @@
+import java.util.concurrent.ConcurrentLinkedDeque;
+
+public class CountDisplay extends Display {
+    public CountDisplay(DisplayImpl impl) {
+        super(impl);
+    }
+    public void multiDisplay(int times){
+        open();
+        for (int i=0;i<times;++i) {
+            print();
+        }
+        close();
+    }
+    
+}
---
+++ src/main/bridge/DisplayImpl.java
@@ -0,0 +1,5 @@
+public abstract class DisplayImpl {
+    public abstract void rawOpen();
+    public abstract void rawClose();
+    public abstract void rawPrint();
+}
